53f2f4358c266ef05b6a976848b8259c22eb8b60
Rohan Atale <atale480@gmail.com>
2020-04-20 15:25:52 +0530

    Survey UI Chnage
    Change in Survey UI.

---
+++ app/src/main/java/com/example/quiz/DashboardActivity.java
@@ -0,0 +1,108 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.quiz.activities.CategoriesActivity;
+import com.example.quiz.activities.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private CardView profile,surveyResponse,takeTest,testHistory,learningPath,setting,rateUs,logOut;
+    private View decorView;
+    private TextView username;
+    private CircularImageView dp;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        // For Full Experince
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if(visibility == 0){
+                    decorView.setSystemUiVisibility(hideNavigationBar());
+                }
+            }
+        });
+
+        // Initilizing CardViews and other Widgets
+        profile = findViewById(R.id.profile);
+        surveyResponse = findViewById(R.id.surveyHistory);
+        takeTest = findViewById(R.id.takeTest);
+        testHistory = findViewById(R.id.testHistory);
+        learningPath = findViewById(R.id.learningPath);
+        setting = findViewById(R.id.setting);
+        rateUs = findViewById(R.id.rateUs);
+        logOut = findViewById(R.id.logOut);
+        username = findViewById(R.id.usrname);
+        dp = findViewById(R.id.profileImage);
+        firebaseAuth= FirebaseAuth.getInstance();
+        final FirebaseUser user=firebaseAuth.getCurrentUser();
+//        Drawable d = getDrawable(R.drawable.person);
+
+        // Setting On ClickListner On Card Views
+        profile.setOnClickListener(this);
+        surveyResponse.setOnClickListener(this);
+        takeTest.setOnClickListener(this);
+        testHistory.setOnClickListener(this);
+        learningPath.setOnClickListener(this);
+        setting.setOnClickListener(this);
+        rateUs.setOnClickListener(this);
+        logOut.setOnClickListener(this);
+
+//            username.setText(user.getDisplayName());
+//            Glide.with(dp.getContext()).load(user.getPhotoUrl()).into(dp);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            decorView.setSystemUiVisibility(hideNavigationBar());
+        }
+    }
+
+    //For Hiding Navigation Bar and Status Bar
+    private int hideNavigationBar(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            //case R.id.profile : break;
+            //case R.id.surveyHistory : break;
+            case R.id.takeTest : i = new Intent(DashboardActivity.this, CategoriesActivity.class);startActivity(i);finish();break;
+            //case R.id.testHistory : break;
+            //case R.id.learningPath : break;
+            //case R.id.setting : break;
+            //case R.id.rateUs : break;
+            case R.id.logOut : i = new Intent(DashboardActivity.this, LoginActivity.class);firebaseAuth.signOut();startActivity(i);finish();break;
+        }
+    }
+}
--- app/src/main/java/com/example/quiz/activities/SurveyActivity.java
+++ app/src/main/java/com/example/quiz/activities/SurveyActivity.java
@@ -1,16 +1,26 @@
 package com.example.quiz.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.animation.Animator;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.quiz.DashboardActivity;
 import com.example.quiz.MainActivity;
 import com.example.quiz.R;
 import com.example.quiz.models.SurveyModel;
@@ -23,14 +33,17 @@ import java.util.List;
 
 public class SurveyActivity extends AppCompatActivity {
 
-    private Spinner experience,expectation,type_of_person,idea,curiosity;
     private List<String> surveydetails;
     private View decorView;
-    private Button submit;
+    private TextView no_indicator,question;
+    private LinearLayout optionsContainer;
+    private Button previous,next;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
     private String surveyid = "";
     private String testhistoryid = "";
+    private int position = 0,count = 0;
+    private String[] question_array,option1,option2,option3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,136 +61,186 @@ public class SurveyActivity extends AppCompatActivity {
             }
         });
 
-        experience = findViewById(R.id.experience);
-        expectation = findViewById(R.id.expectation);
-        type_of_person = findViewById(R.id.type_of_person);
-        idea = findViewById(R.id.idea);
-        curiosity = findViewById(R.id.curiosity);
-        submit = findViewById(R.id.btSubmit);
+        // Linking layout component with variables
+        Toolbar toolbar = findViewById(R.id.surveyheader);
+        setSupportActionBar(toolbar);
+
+        no_indicator = findViewById(R.id.no_indicator);
+        question = findViewById(R.id.question);
+        optionsContainer = findViewById(R.id.options_container);
+        previous = findViewById(R.id.previous);
+        next = findViewById(R.id.next);
         surveydetails = new ArrayList<>();
 
-        // setting data in Spinner
-        setDataInSpinner();
+        question_array = getResources().getStringArray(R.array.question_array);
+        option1 = getResources().getStringArray(R.array.option1);
+        option2 = getResources().getStringArray(R.array.option2);
+        option3 = getResources().getStringArray(R.array.option3);
 
         // setting default data in list
         setDatainList();
 
-        experience.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                surveydetails.set(0,parent.getItemAtPosition(position).toString());
-            }
+        //Display Question
+        displayQuestion();
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+    }
 
-            }
-        });
-
-        expectation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                surveydetails.set(1,parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        type_of_person.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                surveydetails.set(2,parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        idea.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                surveydetails.set(3,parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        curiosity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                surveydetails.set(4,parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        submit.setOnClickListener(new View.OnClickListener() {
+    private void displayQuestion() {
+        // Animating Question Loading
+        playAnime(question,0,question_array[position]);
+        // Handle Listener For Next Button
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                surveyid = myRef.child("SurveyHistory").push().getKey();
-                testhistoryid = myRef.child("TestHistory").push().getKey();
-                String uid = getIntent().getStringExtra("userid");
-                UserModel userModel = new UserModel("0",uid,"1"," ",surveyid,testhistoryid);
-                myRef.child("Users").child(uid).setValue(userModel);
-                SurveyModel surveyModel = new SurveyModel(uid,surveydetails);
-                myRef.child("SurveyHistory").child(surveyid).setValue(surveyModel);
-                Toast.makeText(SurveyActivity.this, "Response Recorded Successfully", Toast.LENGTH_SHORT).show();
-                Intent mainIntent = new Intent(SurveyActivity.this, MainActivity.class);
-                startActivity(mainIntent);
+                handleNextButtonClick();
             }
         });
+        // Handle Listener for Previous Question
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handlePreviousButtonClick();
+            }
+        });
+        // Add Listener to the Options
+        setOptionsListener();
+    }
+
+    private void handlePreviousButtonClick() {
+        enableoption(true);
+        next.setEnabled(true);
+        next.setAlpha(1);
+        position--;
+        if(position == 0){
+            previous.setEnabled(false);
+        }else {
+            previous.setEnabled(true);
+        }
+        count = 0;
+        playAnime(question,0,question_array[position]);
+    }
+
+    private void handleNextButtonClick() {
+        next.setEnabled(false);
+        next.setAlpha(0.7f);
+        enableoption(true);
+        previous.setEnabled(true);
+        position++;
+        if(position == 5){
+            storeInDatabase();
+            nextActivity();
+        }else{
+            count = 0;
+            playAnime(question,0,question_array[position]);
+        }
+
+    }
+
+    private void storeInDatabase() {
+        surveyid = myRef.child("SurveyHistory").push().getKey();
+        testhistoryid = myRef.child("TestHistory").push().getKey();
+        String uid = getIntent().getStringExtra("userid");
+        UserModel userModel = new UserModel("0",uid,"1"," ",surveyid,testhistoryid);
+        myRef.child("Users").child(uid).setValue(userModel);
+        SurveyModel surveyModel = new SurveyModel(uid,surveydetails);
+        myRef.child("SurveyHistory").child(surveyid).setValue(surveyModel);
+        Toast.makeText(SurveyActivity.this, "Response Recorded Successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setOptionsListener() {
+        for(int x = 0; x < 3; x++){
+            final int finalX = x;
+            optionsContainer.getChildAt(x).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    storeanswer((Button) v,finalX);
+                }
+            });
+        }
+    }
+
+    private void playAnime(final View view, final int value, final String data){
+        view.animate().alpha(value).scaleX(value).scaleY(value).setDuration(500).setStartDelay(100)
+                .setInterpolator(new DecelerateInterpolator()).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                if(value == 0 && count < 3){
+                    String option = "";
+                    if(count ==0){
+                        option = option1[position];
+                    }else if(count == 1){
+                        option = option2[position];
+                    }else if(count == 2){
+                        if(position == 0){
+                            optionsContainer.getChildAt(count).setVisibility(View.VISIBLE);
+                            option = option3[position];
+                        }else{
+                            optionsContainer.getChildAt(count).setVisibility(View.INVISIBLE);
+                        }
+                    }
+                    playAnime(optionsContainer.getChildAt(count),0,option);
+                    count++;
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //data change
+                if(value == 0){
+                    try{
+                        ((TextView) view).setText(data);
+                        no_indicator.setText("Question: "+(position+1)+"/5");
+                    }catch (ClassCastException e){
+                        ((Button) view).setText(data);
+                    }
+                    view.setTag(data);
+                    playAnime(view, 1, data);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+    private void nextActivity() {
+        Intent dashboardIntent = new Intent(SurveyActivity.this, DashboardActivity.class);
+        startActivity(dashboardIntent);
+        finish();
+        return;
+    }
+
+    private void storeanswer(Button selectedoption, int index) {
+        selectedoption.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#3BB9FF")));
+        enableoption(false);
+        next.setEnabled(true);
+        next.setAlpha(1);
+        surveydetails.set(position,String.valueOf(index));
+    }
+
+    private void enableoption(boolean enable) {
+        for (int i = 0; i < 3; i++) {
+            optionsContainer.getChildAt(i).setEnabled(enable);
+            if (enable) {
+                optionsContainer.getChildAt(i).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#989898")));
+            }
+        }
     }
 
     private void setDatainList() {
-        surveydetails.add("0 years");
-        surveydetails.add("For the purpose of jobs");
-        surveydetails.add("Do You love Practical Knowledge");
-        surveydetails.add("Own Ideas");
-        surveydetails.add("Learning new Things");
+        surveydetails.add("0");
+        surveydetails.add("0");
+        surveydetails.add("0");
+        surveydetails.add("0");
+        surveydetails.add("0");
     }
 
-    private void setDataInSpinner() {
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.expectation_array, android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        experience.setAdapter(adapter1);
-
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.expectation_array, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        expectation.setAdapter(adapter2);
-
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-                R.array.type_of_person_array, android.R.layout.simple_spinner_item);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        type_of_person.setAdapter(adapter3);
-
-        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
-                R.array.idea_array, android.R.layout.simple_spinner_item);
-        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        idea.setAdapter(adapter4);
-
-        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
-                R.array.curiosity_array, android.R.layout.simple_spinner_item);
-        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        curiosity.setAdapter(adapter5);
-    }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
